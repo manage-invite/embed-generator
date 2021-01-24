97abcb3ccdb632d51fc9bb1ae172b347435b46e6
Androz2091 <androz2091@gmail.com>
2021-01-24 10:27:50 +0100

    :art: Clean up code

--- js/index.js
+++ js/index.js
@@ -12,8 +12,6 @@ $(document).ready(function () {
 
     var fields = 1;
 
-    var source = '';
-
     var embed = {
         title: '',
         author: {
@@ -38,160 +36,59 @@ $(document).ready(function () {
     function updateEmbed(embed) {
         resetEmbed();
 
-        // add basic embed generation to source
-        source = 'embed=discord.Embed(';
-
         if (embed.url) {
-        $('.embed-inner').append('<div class="embed-title"><a href="' + embed.url + '">' + embed.title + '</a></div>');
-
-        // update source
-        if (switches.useVars) {
-            source += 'title=' + embed.title + ', url=' + embed.url;
-        } else {
-            source += 'title="' + embed.title + '", url="' + embed.url + '"';
-        }
-        } else if (embed.title.length === 0) {
-        source += "";
-        } else {
-        $('.embed-inner').append('<div class="embed-title">' + embed.title + '</div>');
-
-        // update source
-        if (switches.useVars) {
-            source += 'title=' + embed.title;
-        } else {
-            source += 'title="' + embed.title + '"';
-        }
-
+            $('.embed-inner').append('<div class="embed-title"><a href="' + embed.url + '">' + embed.title + '</a></div>');
+        } else if (embed.title.length !== 0) {
+            $('.embed-inner').append('<div class="embed-title">' + embed.title + '</div>');
         }
 
         if (embed.description) {
-        $('.embed-inner').append('<div class="embed-description">' + converter.makeHtml(embed.description) + '</div>');
-
-        if (embed.title.length > 0 || embed.url.length > 0) {
-            source += ', '
-        }
-
-        // update source
-        if (switches.useVars) {
-            source += 'description=' + embed.description;
-        } else {
-            source += 'description="' + embed.description + '"';
-        }
+            $('.embed-inner').append('<div class="embed-description">' + converter.makeHtml(embed.description) + '</div>');
         }
 
         if (embed.color) {
-        $('.side-colored').css('background-color', embed.color);
-
-        if (embed.title.length > 0 || embed.url.length > 0) {
-            source += ', '
+            $('.side-colored').css('background-color', embed.color);
         }
-
-        // update source
-        source += 'color=0x' + embed.color.substr(1);
-        }
-
-        // finished the basic setup
-        source += ')\n';
 
         if (embed.author.name) {
-        // add author to source
-        source += 'embed.set_author(';
-
-        $('.embed-title').before('<div class="embed-author"><a class="embed-author-name" href="' + embed.author.url + '">' + embed.author.name + '</a></div>');
-
-        // update source
-        if (switches.useVars) {
-            source += 'name=' + embed.author.name;
-        } else {
-            source += 'name="' + embed.author.name + '"';
-        }
-
-        if(embed.author.url) {
-            source += ', ';
-
-            if (switches.useVars) {
-            source += 'url=' + embed.author.url;
-            } else {
-            source += 'url="' + embed.author.url + '"';
-            }
+            $('.embed-title').before('<div class="embed-author"><a class="embed-author-name" href="' + embed.author.url + '">' + embed.author.name + '</a></div>');
         }
 
         if (embed.author.icon) {
             $('.embed-author-name').before('<img class="embed-author-icon" src="' + embed.author.icon + '" />');
-
-            source += ', ';
-
-            // update source
-            if (switches.useVars) {
-            source += 'icon_url=' + embed.author.icon;
-            } else {
-            source += 'icon_url="' + embed.author.icon + '"';
-            }
-        }
-
-        // finish author
-        source += ')\n';
         }
 
         if (embed.thumb_url) {
         // add thumbnail
-        source += 'embed.set_thumbnail(';
-
-        $('.card.embed .card-block').append('<img class="embed-thumb" src="' + embed.thumb_url + '" />');
-        $('.embed-thumb').height($('.embed-thumb')[0].naturalHeight);
-
-        // update source
-        if (switches.useVars) {
-            source += 'url=' + embed.thumb_url;
-        } else {
-            source += 'url="' + embed.thumb_url + '"';
-        }
-
-        // finish thumbnail
-        source += ')\n';
+            $('.card.embed .card-block').append('<img class="embed-thumb" src="' + embed.thumb_url + '" />');
+            $('.embed-thumb').height($('.embed-thumb')[0].naturalHeight);
         }
 
         if (embed.fields.length > 0) {
-        $('.embed-inner').append('<div class="fields"></div>');
+            $('.embed-inner').append('<div class="fields"></div>');
         }
 
         for (var _iterator = embed.fields, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
-        var _ref;
+            var _ref;
 
-        if (_isArray) {
-            if (_i >= _iterator.length) break;
-            _ref = _iterator[_i++];
-        } else {
-            _i = _iterator.next();
-            if (_i.done) break;
-            _ref = _i.value;
-        }
+            if (_isArray) {
+                if (_i >= _iterator.length) break;
+                _ref = _iterator[_i++];
+            } else {
+                _i = _iterator.next();
+                if (_i.done) break;
+                _ref = _i.value;
+            }
 
-        var field = _ref;
+            var field = _ref;
 
-        $('.embed-inner .fields').append('\n        <div class="field ' + (field.inline && 'inline') + '">\n          <div class="field-name">' + field.name + '</div>\n          <div class="field-value">' + converter.makeHtml(field.value) + '</div>\n        </div>\n      ');
+            $('.embed-inner .fields').append('\n        <div class="field ' + (field.inline && 'inline') + '">\n          <div class="field-name">' + field.name + '</div>\n          <div class="field-value">' + converter.makeHtml(field.value) + '</div>\n        </div>\n      ');
 
-        // add field
-        if (switches.useVars) {
-            source += 'embed.add_field(name=' + field.name + ', value=' + field.value + ', inline=' + (field.inline && 'True' || 'False') + ')\n';
-        } else {
-            source += 'embed.add_field(name="' + field.name + '", value="' + field.value + '", inline=' + (field.inline && 'True' || 'False') + ')\n';
-        }
         }
 
         if (embed.footer) {
-        $('.card.embed').append('<div class="embed-footer"><span>' + embed.footer + '</span></div>');
-
-        // add footer
-        if (switches.useVars) {
-            source += 'embed.set_footer(text=' + embed.footer + ')\n';
-        } else {
-            source += 'embed.set_footer(text="' + embed.footer + '")\n';
+            $('.card.embed').append('<div class="embed-footer"><span>' + embed.footer + '</span></div>');
         }
-        }
-
-        // add send function
-        source += 'await ctx.send(embed=embed)\n';
 
         // code
         $('.json-source').text(JSON.stringify(embed));
@@ -206,32 +103,33 @@ $(document).ready(function () {
         $('.input-fields').html('');
 
         var _loop = function _loop(i) {
-        $('.input-fields').append('<div class="form-group row">\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-name" type="text" placeholder="name" value="' + (embed.fields[i].name !== undefined ? embed.fields[i].name : '') + '" />\n        </div>\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-value" type="text" placeholder="value" value="' + (embed.fields[i].value !== undefined ? embed.fields[i].value : '') + '" />\n        </div>\n        <div class="col-sm-2">\n          <div class="form-check">\n            <label class="form-check-label">\n              <input class="form-check-input" id="field-' + i + '-inline" type="checkbox" ' + (embed.fields[i].inline !== undefined ? 'checked="checked"' : '') + '> Inline\n            </label>\n          </div>\n        </div>\n        <div class="col-sm-2">\n          <button id="field-' + i + '-delete" class="btn btn-danger">Delete</button>\n        </div>\n      </div>');
-        $('#field-' + i + '-name').keyup(function () {
-            updateFieldName(i, $('#field-' + i + '-name').val());
-        });
+            $('.input-fields').append('<div class="form-group row">\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-name" type="text" placeholder="name" value="' + (embed.fields[i].name !== undefined ? embed.fields[i].name : '') + '" />\n        </div>\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-value" type="text" placeholder="value" value="' + (embed.fields[i].value !== undefined ? embed.fields[i].value : '') + '" />\n        </div>\n        <div class="col-sm-2">\n          <div class="form-check">\n            <label class="form-check-label">\n              <input class="form-check-input" id="field-' + i + '-inline" type="checkbox" ' + (embed.fields[i].inline !== undefined ? 'checked="checked"' : '') + '> Inline\n            </label>\n          </div>\n        </div>\n        <div class="col-sm-2">\n          <button id="field-' + i + '-delete" class="btn btn-danger">Delete</button>\n        </div>\n      </div>');
+            $('#field-' + i + '-name').keyup(function () {
+                updateFieldName(i, $('#field-' + i + '-name').val());
+            });
 
-        $('#field-' + i + '-value').keyup(function () {
-            updateFieldValue(i, $('#field-' + i + '-value').val());
-        });
+            $('#field-' + i + '-value').keyup(function () {
+                updateFieldValue(i, $('#field-' + i + '-value').val());
+            });
 
-        $('#field-' + i + '-inline').click(function () {
-            updateFieldInline(i, $('#field-' + i + '-inline').is(':checked'));
-        });
+            $('#field-' + i + '-inline').click(function () {
+                updateFieldInline(i, $('#field-' + i + '-inline').is(':checked'));
+            });
 
-        $('#field-' + i + '-delete').click(function (e) {
-            e.preventDefault();
-            deleteField(i);
-        });
+            $('#field-' + i + '-delete').click(function (e) {
+                e.preventDefault();
+                deleteField(i);
+            });
         };
 
         for (var i = 0; i < fields; i++) {
-        _loop(i);
+            _loop(i);
         }
+
         $('.input-fields').append('<button id="add-field" class="btn btn-success">Add field</button>');
         $('#add-field').click(function (e) {
-        e.preventDefault();
-        addField();
+            e.preventDefault();
+            addField();
         });
     }
 
