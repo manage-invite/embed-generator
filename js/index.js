da86dd24520d14a5f5c5be7be3359c571a950aaa
Androz2091 <androz2091@gmail.com>
2021-01-24 10:39:37 +0100

    :art: Eslint

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
 $(document).ready(function () {
-    const converter = new showdown.Converter();
+    const converter = new showdown.Converter()
 
-    let fields = 1;
+    let fields = 1
 
     const embed = {
         title: '',
@@ -18,285 +18,285 @@ $(document).ready(function () {
         color: '',
         fields: [{}],
         footer: ''
-    };
-
-    function resetEmbed() {
-        $('.embed-inner').html('');
-        $('.embed-footer').remove();
-        $('.embed-thumb').remove();
     }
 
-    function updateEmbed(embed) {
-        resetEmbed();
+    function resetEmbed () {
+        $('.embed-inner').html('')
+        $('.embed-footer').remove()
+        $('.embed-thumb').remove()
+    }
+
+    function updateEmbed (embed) {
+        resetEmbed()
 
         if (embed.url) {
-            $('.embed-inner').append('<div class="embed-title"><a href="' + embed.url + '">' + embed.title + '</a></div>');
+            $('.embed-inner').append('<div class="embed-title"><a href="' + embed.url + '">' + embed.title + '</a></div>')
         } else if (embed.title.length !== 0) {
-            $('.embed-inner').append('<div class="embed-title">' + embed.title + '</div>');
+            $('.embed-inner').append('<div class="embed-title">' + embed.title + '</div>')
         }
 
         if (embed.description) {
-            $('.embed-inner').append('<div class="embed-description">' + converter.makeHtml(embed.description) + '</div>');
+            $('.embed-inner').append('<div class="embed-description">' + converter.makeHtml(embed.description) + '</div>')
         }
 
         if (embed.color) {
-            $('.side-colored').css('background-color', embed.color);
+            $('.side-colored').css('background-color', embed.color)
         }
 
         if (embed.author.name) {
-            $('.embed-title').before('<div class="embed-author"><a class="embed-author-name" href="' + embed.author.url + '">' + embed.author.name + '</a></div>');
+            $('.embed-title').before('<div class="embed-author"><a class="embed-author-name" href="' + embed.author.url + '">' + embed.author.name + '</a></div>')
         }
 
         if (embed.author.icon) {
-            $('.embed-author-name').before('<img class="embed-author-icon" src="' + embed.author.icon + '" />');
+            $('.embed-author-name').before('<img class="embed-author-icon" src="' + embed.author.icon + '" />')
         }
 
         if (embed.thumb_url) {
         // add thumbnail
-            $('.card.embed .card-block').append('<img class="embed-thumb" src="' + embed.thumb_url + '" />');
-            $('.embed-thumb').height($('.embed-thumb')[0].naturalHeight);
+            $('.card.embed .card-block').append('<img class="embed-thumb" src="' + embed.thumb_url + '" />')
+            $('.embed-thumb').height($('.embed-thumb')[0].naturalHeight)
         }
 
         if (embed.fields.length > 0) {
-            $('.embed-inner').append('<div class="fields"></div>');
+            $('.embed-inner').append('<div class="fields"></div>')
         }
 
         embed.fields.forEach((field) => {
-            $('.embed-inner .fields').append('\n        <div class="field ' + (field.inline && 'inline') + '">\n          <div class="field-name">' + field.name + '</div>\n          <div class="field-value">' + converter.makeHtml(field.value) + '</div>\n        </div>\n      ');
+            $('.embed-inner .fields').append('\n        <div class="field ' + (field.inline && 'inline') + '">\n          <div class="field-name">' + field.name + '</div>\n          <div class="field-value">' + converter.makeHtml(field.value) + '</div>\n        </div>\n      ')
         })
 
         if (embed.footer) {
-            $('.card.embed').append('<div class="embed-footer"><span>' + embed.footer + '</span></div>');
+            $('.card.embed').append('<div class="embed-footer"><span>' + embed.footer + '</span></div>')
         }
 
         // code
-        $('.json-source').text(JSON.stringify(embed));
-        hljs.highlightBlock($('.json-source')[0]);
+        $('.json-source').text(JSON.stringify(embed))
+        hljs.highlightBlock($('.json-source')[0])
     }
 
     // run once on startup
-    updateEmbed(embed);
+    updateEmbed(embed)
 
-    function generateInputFields(fields) {
+    function generateInputFields (fields) {
         // generate inputs for fields
-        $('.input-fields').html('');
+        $('.input-fields').html('')
 
-        let _loop = (i) => {
-            $('.input-fields').append('<div class="form-group row">\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-name" type="text" placeholder="name" value="' + (embed.fields[i].name !== undefined ? embed.fields[i].name : '') + '" />\n        </div>\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-value" type="text" placeholder="value" value="' + (embed.fields[i].value !== undefined ? embed.fields[i].value : '') + '" />\n        </div>\n        <div class="col-sm-2">\n          <div class="form-check">\n            <label class="form-check-label">\n              <input class="form-check-input" id="field-' + i + '-inline" type="checkbox" ' + (embed.fields[i].inline !== undefined ? 'checked="checked"' : '') + '> Inline\n            </label>\n          </div>\n        </div>\n        <div class="col-sm-2">\n          <button id="field-' + i + '-delete" class="btn btn-danger">Delete</button>\n        </div>\n      </div>');
+        const _loop = (i) => {
+            $('.input-fields').append('<div class="form-group row">\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-name" type="text" placeholder="name" value="' + (embed.fields[i].name !== undefined ? embed.fields[i].name : '') + '" />\n        </div>\n        <div class="col-sm-4">\n          <input class="form-control" id="field-' + i + '-value" type="text" placeholder="value" value="' + (embed.fields[i].value !== undefined ? embed.fields[i].value : '') + '" />\n        </div>\n        <div class="col-sm-2">\n          <div class="form-check">\n            <label class="form-check-label">\n              <input class="form-check-input" id="field-' + i + '-inline" type="checkbox" ' + (embed.fields[i].inline !== undefined ? 'checked="checked"' : '') + '> Inline\n            </label>\n          </div>\n        </div>\n        <div class="col-sm-2">\n          <button id="field-' + i + '-delete" class="btn btn-danger">Delete</button>\n        </div>\n      </div>')
             $('#field-' + i + '-name').keyup(function () {
-                updateFieldName(i, $('#field-' + i + '-name').val());
-            });
+                updateFieldName(i, $('#field-' + i + '-name').val())
+            })
 
             $('#field-' + i + '-value').keyup(function () {
-                updateFieldValue(i, $('#field-' + i + '-value').val());
-            });
+                updateFieldValue(i, $('#field-' + i + '-value').val())
+            })
 
             $('#field-' + i + '-inline').click(function () {
-                updateFieldInline(i, $('#field-' + i + '-inline').is(':checked'));
-            });
+                updateFieldInline(i, $('#field-' + i + '-inline').is(':checked'))
+            })
 
             $('#field-' + i + '-delete').click(function (e) {
-                e.preventDefault();
-                deleteField(i);
-            });
-        };
-
-        for (let i = 0; i < fields; i++) {
-            _loop(i);
+                e.preventDefault()
+                deleteField(i)
+            })
         }
 
-        $('.input-fields').append('<button id="add-field" class="btn btn-success">Add field</button>');
+        for (let i = 0; i < fields; i++) {
+            _loop(i)
+        }
+
+        $('.input-fields').append('<button id="add-field" class="btn btn-success">Add field</button>')
         $('#add-field').click(function (e) {
-            e.preventDefault();
-            addField();
-        });
+            e.preventDefault()
+            addField()
+        })
     }
 
-    generateInputFields(fields);
+    generateInputFields(fields)
 
-    function updateFieldName(index, value) {
-        embed.fields[index].name = value;
-        updateEmbed(embed);
+    function updateFieldName (index, value) {
+        embed.fields[index].name = value
+        updateEmbed(embed)
     }
 
-    function updateFieldValue(index, value) {
-        embed.fields[index].value = value;
-        updateEmbed(embed);
+    function updateFieldValue (index, value) {
+        embed.fields[index].value = value
+        updateEmbed(embed)
     }
 
-    function updateFieldInline(index, value) {
-        embed.fields[index].inline = value;
-        updateEmbed(embed);
+    function updateFieldInline (index, value) {
+        embed.fields[index].inline = value
+        updateEmbed(embed)
     }
 
-    function deleteField(index) {
-        embed.fields.splice(index, 1);
-        updateEmbed(embed);
-        fields -= 1;
-        generateInputFields(fields);
+    function deleteField (index) {
+        embed.fields.splice(index, 1)
+        updateEmbed(embed)
+        fields -= 1
+        generateInputFields(fields)
     }
 
-    function addField() {
-        embed.fields.push({ inline: true });
-        fields += 1;
-        generateInputFields(fields);
+    function addField () {
+        embed.fields.push({ inline: true })
+        fields += 1
+        generateInputFields(fields)
     }
 
-    function updateTitle(value) {
-        embed.title = value || '';
-        updateEmbed(embed);
+    function updateTitle (value) {
+        embed.title = value || ''
+        updateEmbed(embed)
     }
 
-    function updateUrl(value) {
-        embed.url = value || '';
-        updateEmbed(embed);
+    function updateUrl (value) {
+        embed.url = value || ''
+        updateEmbed(embed)
     }
 
-    function updateThumb(value) {
-        embed.thumb_url = value || false;
-        updateEmbed(embed);
+    function updateThumb (value) {
+        embed.thumb_url = value || false
+        updateEmbed(embed)
     }
 
-    function updateDescription(value) {
-        embed.description = value || '';
-        updateEmbed(embed);
+    function updateDescription (value) {
+        embed.description = value || ''
+        updateEmbed(embed)
     }
 
-    function updateColor(value) {
-        embed.color = value || false;
-        updateEmbed(embed);
+    function updateColor (value) {
+        embed.color = value || false
+        updateEmbed(embed)
     }
 
-    function updateAuthorName(value) {
-        embed.author.name = value || '';
-        updateEmbed(embed);
+    function updateAuthorName (value) {
+        embed.author.name = value || ''
+        updateEmbed(embed)
     }
 
-    function updateAuthorUrl(value) {
-        embed.author.url = value || '';
-        updateEmbed(embed);
+    function updateAuthorUrl (value) {
+        embed.author.url = value || ''
+        updateEmbed(embed)
     }
 
-    function updateAuthorIcon(value) {
-        embed.author.icon = value || '';
-        updateEmbed(embed);
+    function updateAuthorIcon (value) {
+        embed.author.icon = value || ''
+        updateEmbed(embed)
     }
 
-    function updateFooter(value) {
-        embed.footer = value || '';
-        updateEmbed(embed);
+    function updateFooter (value) {
+        embed.footer = value || ''
+        updateEmbed(embed)
     }
 
     $('#form').submit(function (e) {
-        e.preventDefault();
-    });
+        e.preventDefault()
+    })
 
     // checking helpers
-    function addWarning(item, type, message) {
-        item.addClass('form-control-warning');
-        item.removeClass('form-control-success');
-        item.parent().addClass('has-warning');
-        item.parent().removeClass('has-success');
+    function addWarning (item, type, message) {
+        item.addClass('form-control-warning')
+        item.removeClass('form-control-success')
+        item.parent().addClass('has-warning')
+        item.parent().removeClass('has-success')
         if ($('#' + type + '-feedback').length === 0) {
-        item.after('<div class="form-control-feedback" id="' + type + '-feedback">' + message + '</div>');
+            item.after('<div class="form-control-feedback" id="' + type + '-feedback">' + message + '</div>')
         }
     }
 
-    function addSuccess(item, type) {
-        item.removeClass('form-control-warning');
-        item.addClass('form-control-success');
-        item.parent().addClass('has-success');
-        item.parent().removeClass('has-warning');
-        $('#' + type + '-feedback').remove();
+    function addSuccess (item, type) {
+        item.removeClass('form-control-warning')
+        item.addClass('form-control-success')
+        item.parent().addClass('has-success')
+        item.parent().removeClass('has-warning')
+        $('#' + type + '-feedback').remove()
     }
 
     $('#title').keyup(function () {
-        let item = $('#title');
-        let title = item.val();
+        const item = $('#title')
+        const title = item.val()
 
         // update
-        updateTitle(title);
-    });
+        updateTitle(title)
+    })
 
     $('#url').keyup(function () {
-        let item = $('#url');
-        let url = item.val();
+        const item = $('#url')
+        const url = item.val()
 
         if (url.substr(0, 4) !== 'http' && url.length !== 0) {
-        addWarning(item, 'url', 'not a valid url');
+            addWarning(item, 'url', 'not a valid url')
         } else {
-        addSuccess(item, 'url');
-        // update
-        updateUrl(url);
+            addSuccess(item, 'url')
+            // update
+            updateUrl(url)
         }
-    });
+    })
 
     $('#icon').keyup(function () {
-        let item = $('#icon');
-        let icon = item.val();
+        const item = $('#icon')
+        const icon = item.val()
 
-        if (icon.substr(0, 4) !== 'http' && icon.length !== 0 ) {
-        addWarning(item, 'icon', 'not a valid url');
+        if (icon.substr(0, 4) !== 'http' && icon.length !== 0) {
+            addWarning(item, 'icon', 'not a valid url')
         } else {
-        addSuccess(item, 'icon');
-        // update
-        updateThumb(icon);
+            addSuccess(item, 'icon')
+            // update
+            updateThumb(icon)
         }
-    });
+    })
 
     $('#description').keyup(function () {
-        let item = $('#description');
-        let description = item.val();
-        addSuccess(item, 'description');
+        const item = $('#description')
+        const description = item.val()
+        addSuccess(item, 'description')
         // update
-        updateDescription(description);
-    });
+        updateDescription(description)
+    })
 
     $('#color').change(function () {
-        updateColor($('#color').val());
-    });
+        updateColor($('#color').val())
+    })
 
     $('#author_name').keyup(function () {
-        let item = $('#author_name');
-        let author_name = item.val();
+        const item = $('#author_name')
+        const authorName = item.val()
 
-        addSuccess(item, 'author_name');
+        addSuccess(item, 'author_name')
         // update
-        updateAuthorName(author_name);
-    });
+        updateAuthorName(authorName)
+    })
 
     $('#author_url').keyup(function () {
-        let item = $('#author_url');
-        let author_url = item.val();
+        const item = $('#author_url')
+        const authorURL = item.val()
 
-        if (author_url.substr(0, 4) !== 'http' && author_url.length !== 0) {
-        addWarning(item, 'author_url', 'not a valid url');
+        if (authorURL.substr(0, 4) !== 'http' && authorURL.length !== 0) {
+            addWarning(item, 'author_url', 'not a valid url')
         } else {
-        addSuccess(item, 'author_url');
-        // update
-        updateAuthorUrl(author_url);
+            addSuccess(item, 'author_url')
+            // update
+            updateAuthorUrl(authorURL)
         }
-    });
+    })
 
     $('#author_icon').keyup(function () {
-        let item = $('#author_icon');
-        let author_icon = item.val();
+        const item = $('#author_icon')
+        const authorIcon = item.val()
 
-        if (author_icon.substr(0, 4) !== 'http' && author_icon.length !== 0) {
-        addWarning(item, 'author_icon', 'not a valid url');
+        if (authorIcon.substr(0, 4) !== 'http' && authorIcon.length !== 0) {
+            addWarning(item, 'author_icon', 'not a valid url')
         } else {
-        addSuccess(item, 'author_icon');
-        // update
-        updateAuthorIcon(author_icon);
+            addSuccess(item, 'author_icon')
+            // update
+            updateAuthorIcon(authorIcon)
         }
-    });
+    })
 
     $('#footer').keyup(function () {
-        let item = $('#footer');
-        let footer = item.val();
+        const item = $('#footer')
+        const footer = item.val()
 
-        addSuccess(item, 'footer');
+        addSuccess(item, 'footer')
         // update
-        updateFooter(footer);
-    });
-});
+        updateFooter(footer)
+    })
+})
